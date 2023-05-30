80ae287a0c3cc4c3a48612bae2047843595a9b5e
Carlos Villegas <carlos.villegasch@gmail.com>
2023-05-30 13:09:44 -0500

    Replacing class for className, <-- for {/*, --> for */} and adding /# to links into href

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+        {/*Cabecera*/}
+        <header className="header">
+            <div className="logo">
+                <div className="play"></div>
+            </div>
+            <h1>Mis Pelis</h1>
+        </header>
+
+        {/*Barra de navegación*/}
+        <nav className="nav">
+            <ul>
+                <li><a href="/#">Inicio</a></li>
+                <li><a href="/#">Peliculas</a></li>
+                <li><a href="/#">Blog</a></li>
+                <li><a href="/#">Contacto</a></li>
+            </ul>
+        </nav>
+
+        {/*Contenido Principal*/}
+        <section className="content">
+            {/*Aqui van las películas*/}
+            <article className="peli-item">
+                <h3>Desarrollo Web Película</h3>
+                <p className="description">Película tech basada en la vida de un programador peruano</p>
+
+                <button className="edit">Editar</button>
+                <button className="delete">Borrar</button>
+            </article>
+
+            <article className="peli-item">
+                <h3>Desarrollo Web Película</h3>
+                <p className="description">Película tech basada en la vida de un programador peruano</p>
+
+                <button className="edit">Editar</button>
+                <button className="delete">Borrar</button>
+            </article>
+
+            <article className="peli-item">
+                <h3>Desarrollo Web Película</h3>
+                <p className="description">Película tech basada en la vida de un programador peruano</p>
+
+                <button className="edit">Editar</button>
+                <button className="delete">Borrar</button>
+            </article>
+
+            <article className="peli-item">
+                <h3>Desarrollo Web Película</h3>
+                <p className="description">Película tech basada en la vida de un programador peruano</p>
+
+                <button className="edit">Editar</button>
+                <button className="delete">Borrar</button>
+            </article>
+        </section>
+
+        {/*Barra lateral de búsqueda de pelis*/}
+        <aside className="lateral">
+            <div className="search">
+                <h3 className="title">Buscador</h3>
+                <form>
+                    <input type="text" />
+                    <button>Buscar</button>
+                </form>
+            </div>
+        
+            {/*Agregar Películas en la barra lateral*/}
+            <div className="add">
+                <h3 className="title">Añadir Película</h3>
+                <form>
+                    <input type="text" placeholder="Titulo" />
+                    <textarea placeholder="Descripción"></textarea>
+                    <input type="submit" value="Guardar" />
+                </form>
+            </div>
+        </aside>
+
+        {/*FOOTER PIE DE PÁGINA*/}
+        <footer className="footer">
+            &copy; Master en JavaScript ES12 y TypeScript - <a href="https://empresarioencasa.org">Empresario en Casa.org</a>
+        </footer>
     </div>
   );
 }
