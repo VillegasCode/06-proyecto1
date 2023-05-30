e20e32ad33c397da5fb04c7ca33226eec0e4e27c
Carlos Villegas <carlos.villegasch@gmail.com>
2023-05-30 13:29:25 -0500

    Creating Listado.js component with original code from App.js

---
+++ src/components/Listado.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export const Listado = () => {
+  return (
+    // {/*Aqui van las películas*/}
+        <>
+        <article className="peli-item">
+            <h3>Desarrollo Web Película</h3>
+            <p className="description">Película tech basada en la vida de un programador peruano</p>
+
+            <button className="edit">Editar</button>
+            <button className="delete">Borrar</button>
+        </article>
+
+        <article className="peli-item">
+            <h3>Desarrollo Web Película</h3>
+            <p className="description">Película tech basada en la vida de un programador peruano</p>
+
+            <button className="edit">Editar</button>
+            <button className="delete">Borrar</button>
+        </article>
+
+        <article className="peli-item">
+            <h3>Desarrollo Web Película</h3>
+            <p className="description">Película tech basada en la vida de un programador peruano</p>
+
+            <button className="edit">Editar</button>
+            <button className="delete">Borrar</button>
+        </article>
+
+        <article className="peli-item">
+            <h3>Desarrollo Web Película</h3>
+            <p className="description">Película tech basada en la vida de un programador peruano</p>
+
+            <button className="edit">Editar</button>
+            <button className="delete">Borrar</button>
+        </article>
+        </>
+    )
+}
