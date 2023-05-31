205145f76d8a1952c8432f9ec4f0a5d32abba447
Carlos Villegas <carlos.villegasch@gmail.com>
2023-05-30 19:38:07 -0500

    Get Form data for example the title and description and we create the peli object wit an id, titulo, descripcion

--- src/components/Crear.js
+++ src/components/Crear.js
@@ -1,13 +1,46 @@
 import React from 'react'
 
 export const Crear = () => {
+  
+  const titulo = "Añadir Película";
+
+  const conseguirDatosForm = e => {
+    e.preventDefault();
+
+    //Conseguir datos del formulario
+    let target = e.target;
+    let titulo = target.titulo.value;
+    let descripcion = target.descripcion.value;
+
+    //Crear objeto de la pelicula a guardar
+    let peli = {
+      id: new Date().getTime(),
+      titulo,
+      descripcion
+    };
+
+console.log(peli);
+
+  }
+
   return (
     <div className="add">
-        <h3 className="title">Añadir Película</h3>
-        <form>
-            <input type="text" placeholder="Titulo" />
-            <textarea placeholder="Descripción"></textarea>
-            <input type="submit" value="Guardar" />
+        <h3 className="title">{titulo}</h3>
+
+        <form onSubmit={conseguirDatosForm}>
+            <input type="text" 
+                  id='titulo'
+                  name='titulo'
+                  placeholder="Titulo" />
+
+            <textarea
+                  id='descripcion'
+                  name='descripcion'
+                  placeholder="Descripción"></textarea>
+                  
+            <input type="submit"
+                   id="save"
+                   value="Guardar" />
         </form>
     </div>
   )
