dab1c87cbb429c5a7c31ad44a72173be492ce4cf
Carlos Villegas <carlos.villegasch@gmail.com>
2023-05-30 20:34:36 -0500

    Save in local storage through an array

--- src/components/Crear.js
+++ src/components/Crear.js
@@ -27,8 +27,33 @@ export const Crear = () => {
       descripcion
     };
 
+    //Guardar Estado
     setPeliState(peli);
 
+    //Guardar en el almacenamiento local con un método
+    guardarEnStorage(peli);
+
+
+  }
+
+  const guardarEnStorage = peli => {
+    //Conseguir los elementos que ya tenemos en Local Storage
+    let elementos = JSON.parse(localStorage.getItem("pelis"));
+
+    //Comprobar si es un array
+    if(Array.isArray(elementos)){
+      //Añadir dentro del array un elemento nuevo
+      elementos.push(peli);
+    } else {
+      //Crear un array con la nueva peli
+      elementos = [peli];
+    }
+
+    //Guardar en el local storage
+    localStorage.setItem("pelis", JSON.stringify(elementos));
+
+    //Devolver objeto guardado
+    return peli;
   }
 
   return (
