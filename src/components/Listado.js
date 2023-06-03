2c886d3955ee48dbe880f93de0cdecb17581f540
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-03 17:40:04 -0500

    conseguirPeliculas method created and hook useEffect to call that function

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,33 +1,23 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 export const Listado = () => {
-  return (
+  
+    // Método para conseguir las películas
+    const conseguirPeliculas = () => {
+        let peliculas = JSON.parse(localStorage.getItem("pelis"));
+
+        console.log(peliculas);
+    }
+  
+    // HOOK useEffect para llamar a la función cada vez que se cargue la página, colocar corchetes vacíos al final
+    useEffect(() => {
+        console.log("Componentes del listado de películas cargado!!");
+        conseguirPeliculas();
+    }, []);
+  
+    return (
     // {/*Aqui van las películas*/}
         <>
-        <article className="peli-item">
-            <h3>Desarrollo Web Película</h3>
-            <p className="description">Película tech basada en la vida de un programador peruano</p>
-
-            <button className="edit">Editar</button>
-            <button className="delete">Borrar</button>
-        </article>
-
-        <article className="peli-item">
-            <h3>Desarrollo Web Película</h3>
-            <p className="description">Película tech basada en la vida de un programador peruano</p>
-
-            <button className="edit">Editar</button>
-            <button className="delete">Borrar</button>
-        </article>
-
-        <article className="peli-item">
-            <h3>Desarrollo Web Película</h3>
-            <p className="description">Película tech basada en la vida de un programador peruano</p>
-
-            <button className="edit">Editar</button>
-            <button className="delete">Borrar</button>
-        </article>
-
         <article className="peli-item">
             <h3>Desarrollo Web Película</h3>
             <p className="description">Película tech basada en la vida de un programador peruano</p>
