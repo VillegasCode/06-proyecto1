edc007743f61d05e456920aff0f325d43fd07676
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-02 21:12:33 -0500

    Cutting guardarEnStorage method and putting in src/helpers/GuardarEnStorage.js

--- src/components/Crear.js
+++ src/components/Crear.js
@@ -36,26 +36,6 @@ export const Crear = () => {
 
   }
 
-  const guardarEnStorage = peli => {
-    //Conseguir los elementos que ya tenemos en Local Storage
-    let elementos = JSON.parse(localStorage.getItem("pelis"));
-
-    //Comprobar si es un array
-    if(Array.isArray(elementos)){
-      //Añadir dentro del array un elemento nuevo
-      elementos.push(peli);
-    } else {
-      //Crear un array con la nueva peli
-      elementos = [peli];
-    }
-
-    //Guardar en el local storage
-    localStorage.setItem("pelis", JSON.stringify(elementos));
-
-    //Devolver objeto guardado
-    return peli;
-  }
-
   return (
     <div className="add">
         <h3 className="title">{tituloComponente}</h3>
