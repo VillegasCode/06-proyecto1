e8ad6aec4f1c67f3c31379102fa99dedd7e52b4d
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-04 21:19:44 -0500

    Form created with 3 objects like input, textarea and input type submit and a function created named titulo_componente

--- src/components/Editar.js
+++ src/components/Editar.js
@@ -1,7 +1,27 @@
 import React from 'react'
 
 export const Editar = () => {
-  return (
-    <div>Editar</div>
+  
+    const titulo_componente = "Editar película";
+  
+    return (
+    <div className='edit_form'>
+        <h3 className='title'>{titulo_componente}</h3>
+        <form>
+            <input type="text"
+                    name='titulo'
+                    className='titulo_editado'
+                    defaultValue="Título original de la película"
+            />
+
+            <textarea
+                name='descripcion'
+                defaultValue="Descripcion Original"
+                className='descripcion_editada'
+            />
+
+            <input type="submit" className="editar" value="Actualizar" />
+        </form>
+    </div>
   )
 }
