91534d0f0763a97d286d1c0248492df5adb181a2
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-08 17:39:01 -0500

    2 props like peli, conseguirPeliculas are received

--- src/components/Editar.js
+++ src/components/Editar.js
@@ -1,13 +1,43 @@
 import React from 'react'
 
-export const Editar = ({peli}) => {
+export const Editar = ({peli, conseguirPeliculas}) => {
   
     const titulo_componente = "Editar película";
-  
+
+    const guardarEdicion = (e, id) => {
+        e.preventDefault();
+
+       //Conseguir el TARGET del evento en este caso debemos conseguir el formulario
+       let target = e.target;
+
+       //Buscar el índice del objeto de la película a actualizar
+       const pelis_almacenadas = conseguirPeliculas();
+        
+       //Ahora toca buscar el índice de la película
+       const indice = pelis_almacenadas.findIndex(peli => peli.id === id);
+
+       //Crear objeto con el indice obtenido de la variable indice
+       let peli_actualiza = {
+            id,
+            titulo: target.titulo.value,
+            descripcion: target.descripcion.value
+       };
+
+       //Actualizar el elemento con ese índice
+       pelis_almacenadas[indice] = peli_actualiza;
+       
+
+       //Guardar el nuevo array de objetos en el localstorage
+
+       
+        //Actualizar estados
+
+    }
+
     return (
     <div className='edit_form'>
         <h3 className='title'>{titulo_componente}: {peli.titulo}</h3>
-        <form>
+        <form onSubmit={ e => guardarEdicion(e, peli.id)}>
             <input type="text"
                     name='titulo'
                     className='titulo_editado'
